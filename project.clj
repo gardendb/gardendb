16885fcab0a5cf552e8bfabd0fd51827b62f0622
gseaton <greg.seaton@aptpath.com>
2015-04-09 18:08:24 -0700

    added :scm to project.clj per clojars requirement

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[codox "0.8.11"]]
+  :scm {:name "git"
+        :url "https://github.com/gardendb/gardendb"}
   :jvm-opts ["-Xmx2g" "-server"]
   :dependencies [[org.clojure/clojure "1.6.0"]])
